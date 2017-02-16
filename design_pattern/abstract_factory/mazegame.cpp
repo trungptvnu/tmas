4a97b6d0c40429629144a69d10261a8f1efe2516
Tai <taitrananhvn@gmail.com>
2017-02-16 02:12:20 -0800

    add file

---
+++ design_pattern/abstract_factory/mazegame.cpp
@@ -0,0 +1,81 @@
+#include <cstdio>
+using namespace std;
+
+class Maze {
+public:
+	Maze() {
+		printf("create a maze\n");
+	}
+};
+
+class MazeFactory {
+public:
+	static MazeFactory* Instance() { // singleton pattern
+		if (_Instance == 0) {
+			_Instance = new MazeFactory;
+		}
+		return _Instance;
+	}
+
+	virtual Maze* MakeMaze() const {
+		return new Maze;
+	}
+
+	MazeFactory() { // constructor is "public"for abstract factory. If singleton, this should be "protected"
+		printf("MazeFactory::MazeFactory()\n"); 
+	}
+
+private:
+	static MazeFactory* _Instance;	
+
+};
+
+class BombedMazeFactory: public MazeFactory {
+public:
+	BombedMazeFactory() {
+		printf("BombedMazeFactory::BombedMazeFactory()\n");
+	}
+};
+
+class MazeGame {
+public:
+	Maze* CreateMaze() {
+		
+		printf("MazeGame::CreateMaze()\n");
+	    Maze* aMaze = MakeMaze();
+    	return aMaze;
+	}
+
+	Maze* CreateMaze(MazeFactory& factory) {
+		Maze* amaze = factory.MakeMaze();
+		return amaze;
+	}
+
+	// factory method
+	virtual Maze* MakeMaze() const
+    { return new Maze; }
+};
+
+class BombedMazeGame: public MazeGame {
+public:
+	BombedMazeGame() {
+		printf("BombedMazeGame::BombedMazeGame()\n");
+	}
+
+	//override this method, it is a factory method
+	Maze* CreateMaze() {
+		printf("BombedMazeGame MazeGame::CreateMaze()\n");
+	    Maze* aMaze = MakeMaze();
+    	return aMaze;
+	}
+};
+
+
+int main() {
+	MazeGame game;
+	BombedMazeFactory factoryBombed;
+
+	game.CreateMaze(factoryBombed);
+
+	return 0;
+}
--- design_pattern/factory_method/mazegame.cpp
+++ design_pattern/factory_method/mazegame.cpp
@@ -9,25 +9,40 @@ public:
 };
 
 class MazeGame {
-	public:
-		Maze* CreateMaze() {
-			
-			printf("MazeGame::CreateMaze()\n");
-		    Maze* aMaze = MakeMaze();
-	    	return aMaze;
-		}
+public:
+	Maze* CreateMaze() {
+		
+		printf("MazeGame::CreateMaze()\n");
+	    Maze* aMaze = MakeMaze();
+    	return aMaze;
+	}
 
-		// factory method
-		virtual Maze* MakeMaze() const
-        { return new Maze; }
+	// factory method
+	virtual Maze* MakeMaze() const
+    { return new Maze; }
 };
 
+class BombedMazeGame: public MazeGame {
+public:
+	BombedMazeGame() {
+		printf("BombedMazeGame::BombedMazeGame()\n");
+	}
+
+	//override this method, it is a factory method
+	Maze* CreateMaze() {
+		printf("BombedMazeGame MazeGame::CreateMaze()\n");
+	    Maze* aMaze = MakeMaze();
+    	return aMaze;
+	}
+};
 
 int main() {
 
 	MazeGame myGame;
     Maze* m1 = myGame.CreateMaze();
     
+    BombedMazeGame myBombedGame;
+    Maze* m2 = myBombedGame.CreateMaze();
 
 	return 0;
 }
